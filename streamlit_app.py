7d1abd4496870d5ae3b6851dcbadf9da2d1ba95a
FFTampinongkol <fftampinongkol09@gmail.com>
2023-12-21 18:32:19 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,10 @@
 import streamlit as st
 from streamlit.logger import get_logger
 
-LOGGER = get_logger(__name__)
-
-
-def run():
-    st.set_page_config(
-        page_title="| Home",
-        page_icon="👋",)
-    st.write("# Summary")
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
+st.set_page_config(page_title="| Home", page_icon="👋")
+st.write("# Summary")
+st.sidebar.success("Select a demo above.")
+st.markdown(
         """
         I am a computer science lecturer at Universitas Bunda Mulia (UBM) with 5+ years of
         experience in teaching and research. The research topics I cover include Optimization,
@@ -32,5 +25,6 @@ def run():
         - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
     """)
 
-if __name__ == "__main__":
-    run()
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone"))
