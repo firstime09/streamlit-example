859c5e09bf4185712faf041fe7f616144b82f1bf
FFTampinongkol <fftampinongkol09@gmail.com>
2024-01-03 16:35:36 +0700

    up

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,15 +9,15 @@ def main():
     api_options = ('Leaf Diseases Detection', 'Phishing Link Detection')
     select_api = st.selectbox(label='Choose what you want to try:',
                               options=api_options)
-    # page_option = (list(ST_DEMO_LEAF.keys())
-    #                if select_api == 'Leaf Diseases Detection'
-    #                else list(ST_DEMO_PHISHING.keys()))
+    page_option = (list(ST_DEMO_LEAF.keys())
+                   if select_api == 'Leaf Diseases Detection'
+                   else list(ST_DEMO_PHISHING.keys()))
   
     # selected_page = st.selectbox("What would you like to try?", options=page_option)
 
-    demo = (ST_DEMO_PHISHING[select_api]
+    demo = (ST_DEMO_PHISHING[page_option]
             if select_api == 'Phishing Link Detection'
-            else ST_DEMO_LEAF[select_api])
+            else ST_DEMO_LEAF[page_option])
   demo()
 
 if __name__ == "__main__":
