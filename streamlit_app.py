46ab4911ef01ba00b19745cd230843f2be1dfced
FFTampinongkol <fftampinongkol09@gmail.com>
2024-01-03 17:23:33 +0700

    up

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,26 +3,6 @@ from demo_leafDiseases import ST_DEMO_LEAF
 from demo_phishingLink import ST_DEMO_PHISHING
 from streamlit.logger import get_logger
 
-st.set_page_config(page_title="| Home", page_icon="👋")
-st.write("# Summary")
-st.markdown(
-        """
-        I am a computer science lecturer at Universitas Bunda Mulia (UBM) with 5+ years of
-        experience in teaching and research. The research topics I cover include Optimization,
-        Image Processing, Expert Systems, Machine Learning, and Artificial Intelligence.
-        The conclusions of all of my research are published in internationally accredited
-        journals, national journals, or conferences and hope to be feedback for review,
-        replicated, or applied in an organization. Some demo systems that have been created
-        can be **viewed on the sidebar 👈**
-        
-        ### Whare you can find me?
-        - in my publication [Google scholar](https://scholar.google.com/citations?user=LxbIO5sAAAAJ)
-        - in my code documentation [Github](https://github.com/firstime09)
-        - in my social media [LinkedIn](https://id.linkedin.com/in/felliks-feiters-tampinongkol-100084174)
-        
-        ### Under Maintenance    
-        """)
-
 def main():
   with st.sidebar:
     st.header('My Prototipe List')
@@ -43,3 +23,23 @@ def main():
 
 if __name__ == "__main__":
   main()
+
+  st.set_page_config(page_title="| Home", page_icon="👋")
+  st.write("# Summary")
+  st.markdown(
+        """
+        I am a computer science lecturer at Universitas Bunda Mulia (UBM) with 5+ years of
+        experience in teaching and research. The research topics I cover include Optimization,
+        Image Processing, Expert Systems, Machine Learning, and Artificial Intelligence.
+        The conclusions of all of my research are published in internationally accredited
+        journals, national journals, or conferences and hope to be feedback for review,
+        replicated, or applied in an organization. Some demo systems that have been created
+        can be **viewed on the sidebar 👈**
+        
+        ### Whare you can find me?
+        - in my publication [Google scholar](https://scholar.google.com/citations?user=LxbIO5sAAAAJ)
+        - in my code documentation [Github](https://github.com/firstime09)
+        - in my social media [LinkedIn](https://id.linkedin.com/in/felliks-feiters-tampinongkol-100084174)
+        
+        ### Under Maintenance    
+        """)
