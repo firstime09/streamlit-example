6dfebc3335468e946d159959c6de13abaef9a2bb
FFTampinongkol <fftampinongkol09@gmail.com>
2024-01-03 17:07:00 +0700

    up

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,29 @@
 import streamlit as st
-from demo_leafDiseases import ST_DEMO_LEAF
-from demo_phishingLink import ST_DEMO_PHISHING
-from streamlit.logger import get_logger
+# from demo_leafDiseases import ST_DEMO_LEAF
+# from demo_phishingLink import ST_DEMO_PHISHING
+# from streamlit.logger import get_logger
 
 def main_page():
+    st.set_page_config(page_title="| Home", page_icon="👋")
+    st.write("# Summary")
     st.markdown("# Main page 🎈")
+    st.markdown(
+        """
+        I am a computer science lecturer at Universitas Bunda Mulia (UBM) with 5+ years of
+        experience in teaching and research. The research topics I cover include Optimization,
+        Image Processing, Expert Systems, Machine Learning, and Artificial Intelligence.
+        The conclusions of all of my research are published in internationally accredited
+        journals, national journals, or conferences and hope to be feedback for review,
+        replicated, or applied in an organization. Some demo systems that have been created
+        can be **viewed on the sidebar 👈**
+        
+        ### Whare you can find me?
+        - in my publication [Google scholar](https://scholar.google.com/citations?user=LxbIO5sAAAAJ)
+        - in my code documentation [Github](https://github.com/firstime09)
+        - in my social media [LinkedIn](https://id.linkedin.com/in/felliks-feiters-tampinongkol-100084174)
+        
+        ### Under Maintenance    
+        """)
     st.sidebar.markdown("# Main page 🎈")
 
 def page2():
@@ -24,49 +43,49 @@ page_names_to_funcs = {
 selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
 page_names_to_funcs[selected_page]()
 
-st.set_page_config(page_title="| Home", page_icon="👋")
-st.write("# Summary")
-#st.sidebar.success("Select a demo above.")
-st.markdown(
-        """
-        I am a computer science lecturer at Universitas Bunda Mulia (UBM) with 5+ years of
-        experience in teaching and research. The research topics I cover include Optimization,
-        Image Processing, Expert Systems, Machine Learning, and Artificial Intelligence.
-        The conclusions of all of my research are published in internationally accredited
-        journals, national journals, or conferences and hope to be feedback for review,
-        replicated, or applied in an organization. Some demo systems that have been created
-        can be **viewed on the sidebar 👈**
+# st.set_page_config(page_title="| Home", page_icon="👋")
+# st.write("# Summary")
+# #st.sidebar.success("Select a demo above.")
+# st.markdown(
+#         """
+#         I am a computer science lecturer at Universitas Bunda Mulia (UBM) with 5+ years of
+#         experience in teaching and research. The research topics I cover include Optimization,
+#         Image Processing, Expert Systems, Machine Learning, and Artificial Intelligence.
+#         The conclusions of all of my research are published in internationally accredited
+#         journals, national journals, or conferences and hope to be feedback for review,
+#         replicated, or applied in an organization. Some demo systems that have been created
+#         can be **viewed on the sidebar 👈**
         
-        ### Whare you can find me?
-        - in my publication [Google scholar](https://scholar.google.com/citations?user=LxbIO5sAAAAJ)
-        - in my code documentation [Github](https://github.com/firstime09)
-        - in my social media [LinkedIn](https://id.linkedin.com/in/felliks-feiters-tampinongkol-100084174)
+#         ### Whare you can find me?
+#         - in my publication [Google scholar](https://scholar.google.com/citations?user=LxbIO5sAAAAJ)
+#         - in my code documentation [Github](https://github.com/firstime09)
+#         - in my social media [LinkedIn](https://id.linkedin.com/in/felliks-feiters-tampinongkol-100084174)
         
-        ### Under Maintenance    
-        """)
+#         ### Under Maintenance    
+#         """)
 
-# def main():
-#   with st.sidebar:
-#     st.header('My Prototipe List')
-#     api_options = ('Leaf Diseases Detection', 'Phishing Link Detection')
-#     select_api = st.selectbox(label='Choose what you want to try:',
-#                               options=api_options)
+# # def main():
+# #   with st.sidebar:
+# #     st.header('My Prototipe List')
+# #     api_options = ('Leaf Diseases Detection', 'Phishing Link Detection')
+# #     select_api = st.selectbox(label='Choose what you want to try:',
+# #                               options=api_options)
     
-#     if select_api == 'Leaf Diseases Detection':
-#       ST_DEMO_LEAF.keys()
-#     else: 
-#       ST_DEMO_PHISHING.keys()
+# #     if select_api == 'Leaf Diseases Detection':
+# #       ST_DEMO_LEAF.keys()
+# #     else: 
+# #       ST_DEMO_PHISHING.keys()
 
-#     # page_option = (list(ST_DEMO_LEAF.keys())
-#     #                if select_api == 'Leaf Diseases Detection'
-#     #                else list(ST_DEMO_PHISHING.keys()))
+# #     # page_option = (list(ST_DEMO_LEAF.keys())
+# #     #                if select_api == 'Leaf Diseases Detection'
+# #     #                else list(ST_DEMO_PHISHING.keys()))
   
-#     # # selected_page = st.selectbox("What would you like to try?", options=page_option)
+# #     # # selected_page = st.selectbox("What would you like to try?", options=page_option)
 
-#     # demo = (ST_DEMO_PHISHING[page_option]
-#     #         if select_api == 'Phishing Link Detection'
-#     #         else ST_DEMO_LEAF[page_option])
-#   demo()
+# #     # demo = (ST_DEMO_PHISHING[page_option]
+# #     #         if select_api == 'Phishing Link Detection'
+# #     #         else ST_DEMO_LEAF[page_option])
+# #   demo()
 
-# if __name__ == "__main__":
-#   main()
+# # if __name__ == "__main__":
+# #   main()
