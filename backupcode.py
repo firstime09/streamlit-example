cf9a4e5518393cf772cbc37807b2a1d2400f9bc8
FFTampinongkol <fftampinongkol09@gmail.com>
2024-01-03 17:32:06 +0700

    up

---
+++ backupcode.py
@@ -0,0 +1,45 @@
+import streamlit as st
+from demo_leafDiseases import ST_DEMO_LEAF
+from demo_phishingLink import ST_DEMO_PHISHING
+from streamlit.logger import get_logger
+
+def main():
+  with st.sidebar:
+    st.header('My Prototipe List')
+    api_options = ('Leaf Diseases Detection', 'Phishing Link Detection')
+    select_api = st.selectbox(label='Choose what you want to try:',
+                              options=api_options)
+    
+    page_option = (list(ST_DEMO_LEAF.keys())
+                   if select_api == 'Leaf Diseases Detection'
+                   else list(ST_DEMO_PHISHING.keys()))
+  
+    # selected_page = st.selectbox("What would you like to try?", options=page_option)
+
+    demo = (ST_DEMO_PHISHING[page_option]
+            if select_api == 'Phishing Link Detection'
+            else ST_DEMO_LEAF[page_option])
+  demo()
+
+if __name__ == "__main__":
+  main()
+
+  st.set_page_config(page_title="| Home", page_icon="👋")
+  st.write("# Summary")
+  st.markdown(
+        """
+        I am a computer science lecturer at Universitas Bunda Mulia (UBM) with 5+ years of
+        experience in teaching and research. The research topics I cover include Optimization,
+        Image Processing, Expert Systems, Machine Learning, and Artificial Intelligence.
+        The conclusions of all of my research are published in internationally accredited
+        journals, national journals, or conferences and hope to be feedback for review,
+        replicated, or applied in an organization. Some demo systems that have been created
+        can be **viewed on the sidebar 👈**
+        
+        ### Whare you can find me?
+        - in my publication [Google scholar](https://scholar.google.com/citations?user=LxbIO5sAAAAJ)
+        - in my code documentation [Github](https://github.com/firstime09)
+        - in my social media [LinkedIn](https://id.linkedin.com/in/felliks-feiters-tampinongkol-100084174)
+        
+        ### Under Maintenance    
+        """)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,45 +1,70 @@
 import streamlit as st
+import inspect
+import textwrap
+
 from demo_leafDiseases import ST_DEMO_LEAF
 from demo_phishingLink import ST_DEMO_PHISHING
-from streamlit.logger import get_logger
+
 
 def main():
-  with st.sidebar:
-    st.header('My Prototipe List')
-    api_options = ('Leaf Diseases Detection', 'Phishing Link Detection')
-    select_api = st.selectbox(label='Choose what you want to try:',
-                              options=api_options)
-    
-    page_option = (list(ST_DEMO_LEAF.keys())
-                   if select_api == 'Leaf Diseases Detection'
-                   else list(ST_DEMO_PHISHING.keys()))
-  
-    # selected_page = st.selectbox("What would you like to try?", options=page_option)
+    st.title("Streamlit ECharts Demo")
 
-    demo = (ST_DEMO_PHISHING[page_option]
-            if select_api == 'Phishing Link Detection'
-            else ST_DEMO_LEAF[page_option])
-  demo()
+    with st.sidebar:
+        st.header("Configuration")
+        api_options = ('Leaf Diseases Detection', 'Phishing Link Detection')
+        selected_api = st.selectbox(
+            label="Choose your preferred API:",
+            options=api_options,
+        )
+
+        page_options = (
+            list(ST_DEMO_PHISHING.keys())
+            if selected_api == 'Phishing Link Detection'
+            else list(ST_DEMO_LEAF.keys())
+        )
+        selected_page = st.selectbox(
+            label="Choose an example",
+            options=page_options,
+        )
+        demo, url = (
+            ST_DEMO_LEAF[selected_page]
+            if selected_api == 'Leaf Diseases Detection'
+            else ST_DEMO_PHISHING[selected_page]
+        )
+
+        if selected_api == "Leaf Diseases Detection":
+            st.caption(
+                """ECharts demos are extracted from https://echarts.apache.org/examples/en/index.html, 
+            by copying/formattting the 'option' json object into st_echarts.
+            Definitely check the echarts example page, convert the JSON specs to Python Dicts and you should get a nice viz."""
+            )
+        if selected_api == "Phishing Link Detection":
+            st.caption(
+                """Pyecharts demos are extracted from https://github.com/pyecharts/pyecharts-gallery,
+            by copying the pyecharts object into st_pyecharts. 
+            Pyecharts is still using ECharts 4 underneath, which is why the theming between st_echarts and st_pyecharts is different."""
+            )
+
+    demo()
+
+    sourcelines, _ = inspect.getsourcelines(demo)
+    with st.expander("Source Code"):
+        st.code(textwrap.dedent("".join(sourcelines[1:])))
+    st.markdown(f"Credit: {url}")
+
 
 if __name__ == "__main__":
-  main()
-
-  st.set_page_config(page_title="| Home", page_icon="👋")
-  st.write("# Summary")
-  st.markdown(
-        """
-        I am a computer science lecturer at Universitas Bunda Mulia (UBM) with 5+ years of
-        experience in teaching and research. The research topics I cover include Optimization,
-        Image Processing, Expert Systems, Machine Learning, and Artificial Intelligence.
-        The conclusions of all of my research are published in internationally accredited
-        journals, national journals, or conferences and hope to be feedback for review,
-        replicated, or applied in an organization. Some demo systems that have been created
-        can be **viewed on the sidebar 👈**
-        
-        ### Whare you can find me?
-        - in my publication [Google scholar](https://scholar.google.com/citations?user=LxbIO5sAAAAJ)
-        - in my code documentation [Github](https://github.com/firstime09)
-        - in my social media [LinkedIn](https://id.linkedin.com/in/felliks-feiters-tampinongkol-100084174)
-        
-        ### Under Maintenance    
-        """)
+    st.set_page_config(
+        page_title="Streamlit ECharts Demo", page_icon=":chart_with_upwards_trend:"
+    )
+    main()
+    with st.sidebar:
+        st.markdown("---")
+        st.markdown(
+            '<h6>Made in &nbsp<img src="https://streamlit.io/images/brand/streamlit-mark-color.png" alt="Streamlit logo" height="16">&nbsp by <a href="https://twitter.com/andfanilo">@andfanilo</a></h6>',
+            unsafe_allow_html=True,
+        )
+        st.markdown(
+            '<div style="margin-top: 0.75em;"><a href="https://www.buymeacoffee.com/andfanilo" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/default-orange.png" alt="Buy Me A Coffee" height="41" width="174"></a></div>',
+            unsafe_allow_html=True,
+        )
