5f3a2f786d5c5f7273a199f944835ee9f4d45b45
FFTampinongkol <fftampinongkol09@gmail.com>
2024-01-04 10:22:56 +0700

    up

--- app/fe_LEDs.py
+++ app/fe_LEDs.py
@@ -1,7 +1,7 @@
 import streamlit as st
-from sklearn import datasets
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+# from sklearn import datasets
+# from sklearn.model_selection import train_test_split
+# from sklearn.ensemble import RandomForestClassifier
 
 def app():
     st.title('Leaf Diseases Detection')
@@ -13,15 +13,15 @@ def app():
             about `Leaf Diseases Segmentation`.
         """)
 
-    iris = datasets.load_iris()
-    X = iris.data
-    Y = iris.target
+    # iris = datasets.load_iris()
+    # X = iris.data
+    # Y = iris.target
 
-    st.header('Model performance')
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2,
-                                                        random_state=42)
-    clf = RandomForestClassifier()
-    clf.fit(X_train, Y_train)
-    score = clf.score(X_test, Y_test)
-    st.write('Accuracy:')
-    st.write(score)
+    # st.header('Model performance')
+    # X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2,
+    #                                                     random_state=42)
+    # clf = RandomForestClassifier()
+    # clf.fit(X_train, Y_train)
+    # score = clf.score(X_test, Y_test)
+    # st.write('Accuracy:')
+    # st.write(score)
