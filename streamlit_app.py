1aa6bccdc443ee7f60bf9e1386aefd829bc46fb4
FFTampinongkol <fftampinongkol09@gmail.com>
2024-01-04 14:12:45 +0700

    up

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ from multiapp import MultiApp
 from app import home, fe_LEDs, fe_LIPs
 
 app = MultiApp()
-st.set_page_config(page_title='| Home Page', page_icon='👋')
+st.set_page_config(page_title='| fftampinongkol', page_icon='👋')
 
 app.add_app("My Home Page", home.app)
 app.add_app("Leaf Diseases Detection", fe_LEDs.app)
